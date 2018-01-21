78e80273e9b259842075b8efcb537af490a28d9a
thiru <a0130737@u.nus.edu>
2018-01-21 22:13:51 +0800

    Added different password logging

--- ivle_token_generator.py
+++ ivle_token_generator.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
 import requests
+import logging
+
+logger = logging.getLogger(__name__)
 
 # Consider changing this to OOP style, but no benefit as of now
 
 #==============================================================================
 #                      Gets auth token from IVLE
-# 
+#
 #==============================================================================
 
-## Main method.    
+## Main method.
 def get_token(API_KEY,IVLE_LOGIN,IVLE_PASS,headers):
     viewstate = get_and_strip_viewstate(API_KEY,IVLE_LOGIN,IVLE_PASS,headers)
     URL = 'https://ivle.nus.edu.sg/api/login/?apikey='+API_KEY
@@ -22,6 +25,7 @@ def get_token(API_KEY,IVLE_LOGIN,IVLE_PASS,headers):
         token = x.text
         if validate_token(API_KEY,token,headers):
             return token
+    logger.error("No token from LAPI received! Are your username and password correct?")
     return
 
 #ASP.net uses viewstates for legit logins. just get it via python instead ;)
@@ -29,7 +33,7 @@ def get_and_strip_viewstate(API_KEY,IVLE_LOGIN,IVLE_PASS,headers):
     xml = getXML(API_KEY,IVLE_LOGIN,IVLE_PASS,headers)
     token = strip_token(xml)
     return token
-    
+
 def getXML(API_KEY,IVLE_LOGIN,IVLE_PASS,headers):
     URL = 'https://ivle.nus.edu.sg/api/login/?apikey=' + API_KEY +'&url=localhost'
     x = requests.get(URL,auth = (IVLE_LOGIN,IVLE_PASS),headers=headers)
@@ -43,7 +47,7 @@ def strip_token(xml):
     endIdx = token_half.find('"')
     token = token_half[:endIdx]
     return token
-    
+
 def validate_token(API_KEY,TOKEN,headers):
     URL = 'https://ivle.nus.edu.sg/api/Lapi.svc/Validate?APIKey='+API_KEY+'&Token='+TOKEN
     x = requests.get(URL,headers=headers).json()
